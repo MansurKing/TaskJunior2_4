c02a2d167791ce9a9bd317a48218b417fe1c76dd
MansurKing <102825225+MansurKing@users.noreply.github.com>
2024-08-19 16:10:47 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TaskJunior2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+
+            int firstValue = 0;
+            int finishValue = 101;
+            int numberDivisibleFirst = 3;
+            int numberDivisibleFirstSecond = 5;
+            int number = rand.Next(firstValue, finishValue);
+            int sum = 0;
+
+            Console.WriteLine(number);
+
+            for (int i = number; i > firstValue; i--)
+            {
+                if ((i % numberDivisibleFirst == 0) || (i % numberDivisibleFirstSecond == 0))
+                {
+                    sum += i;
+                }
+            }
+
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
